d71e46b182ee9addfc5b99fd20bd5a0e4f681400
Horoz <serelaw@mail.ru>
2024-02-07 18:00:25 +0300

    Save commit

--- src/main/java/com/example/katarsisblog/controllers/BlogController.java
+++ src/main/java/com/example/katarsisblog/controllers/BlogController.java
@@ -5,11 +5,7 @@ import com.example.katarsisblog.repo.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Optional;
@@ -23,30 +19,60 @@ public class BlogController {
     public String blogMain(Model model) {
         Iterable<Post> posts = postRepository.findAll();
         model.addAttribute("posts", posts);
-        return "blog-main";
+        return "blog/blog-main";
     }
 
     @GetMapping("/blog/add")
     public String blogAdd(Model model) {
-        return "blog-add";
+        return "blog/blog-add";
     }
 
     @PostMapping("/blog/add")
     public String blogPostAdd(@RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model) {
-        Post post = new Post(title,anons,full_text);
+        Post post = new Post(title, anons, full_text);
         postRepository.save(post);
         return "redirect:/blog";
     }
 
     @GetMapping("/blog/{id}")
     public String blogDetails(@PathVariable(value = "id") long id, Model model) {
-        if (!postRepository.existsById(id)){
+        if (!postRepository.existsById(id)) {
             return "redirect:/blog";
         }
         Optional<Post> post = postRepository.findById(id);
         ArrayList<Post> result = new ArrayList<>();
         post.ifPresent(result::add);
         model.addAttribute("post", result);
-        return "blog-details";
+        return "blog/blog-details";
     }
+
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit(@PathVariable(value = "id") long id, Model model) {
+        if (!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> result = new ArrayList<>();
+        post.ifPresent(result::add);
+        model.addAttribute("post", result);
+        return "blog/blog-edit";
+    }
+
+    @PutMapping("/blog/{id}/edit")
+    public String blogPostUpdate(@PathVariable(value = "id") long id, @RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model) {
+        Post post = postRepository.findById(id).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+    @DeleteMapping("/blog/{id}/remove")
+    public String blogPostDelete(@PathVariable(value = "id") long id) {
+        Post post = postRepository.findById(id).orElseThrow();
+        postRepository.delete(post);
+        return "redirect:/blog";
+    }
+
 }
---
+++ src/main/java/com/example/katarsisblog/controllers/RegistrationController.java
@@ -0,0 +1,13 @@
+package com.example.katarsisblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegistrationController {
+    @GetMapping("/registration")
+    public String registration(Model model) {
+        return "registration";
+    }
+}
